d649b77d45869782642509308e6f0c5b932ce400
thalesvdcolle <thales.vdcolle@gmail.com>
2023-08-21 14:45:34 -0300

    Finalizado exercicio 1

--- app/src/main/java/com/github/thalesvdcolle/exercicio_kg_______lbs/MainActivity.kt
+++ app/src/main/java/com/github/thalesvdcolle/exercicio_kg_______lbs/MainActivity.kt
@@ -2,10 +2,35 @@ package com.github.thalesvdcolle.exercicio_kg_______lbs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.thalesvdcolle.exercicio_kg_______lbs.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.kilos_para_lbs -> 2.2
+            else -> 0.45
+        }
+        var tip = tipPercentage * cost
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
+
