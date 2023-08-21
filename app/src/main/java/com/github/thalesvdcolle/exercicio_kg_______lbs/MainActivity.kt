5d276349283cccfc2ad69af2815343d1cb2d9b01
thalesvdcolle <thales.vdcolle@gmail.com>
2023-08-21 14:11:52 -0300

    Arquivo inicial

---
+++ app/src/main/java/com/github/thalesvdcolle/exercicio_kg_______lbs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.thalesvdcolle.exercicio_kg_______lbs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
